0890e51957d89a0bfbeb22434825f75729370f39
lesliecodes1 <lsltry404@gmail.com>
2023-06-26 19:05:16 +0300

    Added main page content; styled section better

--- app/characters/[id]/page.tsx
+++ app/characters/[id]/page.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+"use client";
 
-export default function Details() {
-  return <div>Details</div>;
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
 }
+
+const CharacterPage: React.FC = () => {
+  const [pokemon, setPokemon] = useState<
+    Array<{ id: number; name: string; image: string }>
+  >([]);
+
+  useEffect(() => {
+    async function getPokemon() {
+      const resp = await fetch(
+        "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+      );
+      setPokemon(await resp.json());
+    }
+    getPokemon();
+  }, []);
+  const router = useRouter();
+  const { id } = router.query;
+  const selectedPokemon = pokemon.find((p) => p.id === Number(id));
+
+  return (
+    <div>
+      <h1>Character Page - ID: {id}</h1>
+      {selectedPokemon && (
+        <div>
+          <h2>{selectedPokemon.name}</h2>
+          <img src={selectedPokemon.image} alt={selectedPokemon.name} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CharacterPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import styles from "../styles/page.module.css";
 import Link from "next/link";
 
@@ -15,6 +16,17 @@ export default function Home() {
           </a>
         </nav>
       </div>
+      <section>
+        Welcome to the captivating world of Pokémon! Prepare to embark on an
+        extraordinary journey filled with adventure, friendship, and thrilling
+        battles. At our Pokémon website, we bring you all the latest news, tips,
+        and insights about the fascinating Pokémon universe. Discover a treasure
+        trove of information about your favorite Pokémon species. From Pikachu's
+        electrifying powers to Charizard's blazing flames, we delve into the
+        unique abilities, characteristics, and evolutions of each Pokémon.
+        Uncover their hidden secrets, learn how to train them effectively, and
+        become a true Pokémon Master.
+      </section>
       <div className={styles.main}>
         <Link legacyBehavior href="/characters">
           <a className={styles.fancy}>
@@ -25,6 +37,19 @@ export default function Home() {
           </a>
         </Link>
       </div>
+      <section>
+        Are you ready to become part of a vibrant community of trainers? Join
+        our forums and connect with fellow Pokémon enthusiasts from around the
+        world. Share your strategies, exchange rare Pokémon, and engage in
+        lively discussions about the latest games, episodes, and movies. From
+        nostalgic veterans to curious newcomers, everyone is welcome to share
+        their love for these incredible creatures. Whether you're a fan of the
+        Pokémon games, the animated series, the trading card game, or all of the
+        above, our website is your one-stop destination for all things Pokémon.
+        Prepare to embark on an extraordinary adventure, make lifelong friends,
+        and experience the magic that has captured the hearts of millions of
+        trainers worldwide.
+      </section>
     </main>
   );
 }
